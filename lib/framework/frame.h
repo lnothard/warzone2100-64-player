ac8a010e34d49f7b2b7d2caee09e66a0e846cf5c
Lucas Nothard <l.nothard@lancaster.ac.uk>
2020-10-02 16:47:50 +0100

    max_players up to 32

--- lib/framework/frame.h
+++ lib/framework/frame.h
@@ -60,8 +60,8 @@
 
 extern uint32_t selectedPlayer;      ///< The player number corresponding to this client.
 extern uint32_t realSelectedPlayer;  ///< The player number corresponding to this client (same as selectedPlayer, unless changing players in the debug menu).
-#define MAX_PLAYERS         17                 ///< Maximum number of players in the game.
-#define MAX_PLAYERS_IN_GUI  16  ///< One player reserved for scavengers.
+#define MAX_PLAYERS         33              ///< Maximum number of players in the game.
+#define MAX_PLAYERS_IN_GUI  32  ///< One player reserved for scavengers.
 #define PLAYER_FEATURE      (MAX_PLAYERS + 1)
 #define MAX_PLAYER_SLOTS    (MAX_PLAYERS + 2)  ///< Max players plus 1 baba and 1 reserved for features. Actually, if baba is a regular player, then it's plus 1 unused?
 
@@ -71,8 +71,8 @@ typedef uint8_t PlayerMask;
 typedef uint16_t PlayerMask;
 #elif MAX_PLAYERS <= 32
 typedef uint32_t PlayerMask;
-#else
-#error Warzone 2100 is not a MMO.
+#elif MAX_PLAYERS <= 64
+typedef uint64_t PlayerMask;
 #endif
 
 enum QUEUE_MODE
